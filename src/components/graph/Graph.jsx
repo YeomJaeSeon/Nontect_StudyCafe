36c6a6ffbb305069b71d5d7d468375762a1761b2
YeomJaeSeon <a89541457@gmail.com>
2021-08-27 19:15:16 +0900

    chore: rechart install,그래프에 데이터 결합

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -1,10 +1,61 @@
 import React, { useEffect, useState } from "react";
-import "../../../node_modules/react-vis/dist/style.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
 import * as S from "./Graph.style";
+
+// const dataV = [
+//   {
+//     name: "Page A",
+
+//     집중도: 86.66666,
+//   },
+//   {
+//     name: "Page B",
+
+//     집중도: 20.3,
+//   },
+//   {
+//     name: "Page C",
+//     // uv: 2000,
+//     집중도: 2800,
+//     // amt: 2290,
+//   },
+//   {
+//     name: "Page D",
+//     // uv: 2780,
+//     집중도: 3908,
+//     // amt: 2000,
+//   },
+//   {
+//     name: "Page E",
+//     // uv: 1890,
+//     집중도: 4800,
+//     // amt: 2181,
+//   },
+//   {
+//     name: "Page F",
+//     // uv: 2390,
+//     집중도: 3800,
+//     // amt: 2500,
+//   },
+//   {
+//     name: "Page G",
+//     // uv: 3490,
+//     집중도: 4300,
+//     // amt: 2100,
+//   },
+// ];
 
 export default function Graph({ dataService, roomName }) {
   const [data, setData] = useState([]);
-  const [users, setUsers] = useState({});
 
   useEffect(() => {
     dataService.getAllUsers((value) => {
@@ -22,37 +73,36 @@ export default function Graph({ dataService, roomName }) {
 
         setData(
           filterArr.map((value, idx) => ({
-            x: idx,
-            y: (value.focus / value.total) * 10,
+            name: value.name,
+            집중도: (value.focus / value.total) * 100,
           }))
         );
-        const entries = new Map();
-        filterArr.forEach((value, idx) => {
-          entries.set(idx, value.name);
-        });
-        const obj = Object.fromEntries(entries);
-        setUsers(obj);
-        console.log("obj name");
-        console.log(obj);
-        console.log(filterArr);
       }
     });
   }, []);
-  const datas = [
-    { x: 0, y: 8 },
-    { x: 1, y: 5 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 1 },
-    { x: 5, y: 7 },
-    { x: 6, y: 6 },
-    { x: 7, y: 3 },
-    { x: 8, y: 2 },
-    { x: 9, y: 5 },
-  ];
+
   return (
     <S.GraphBox className="App">
       <S.GraphTitle>집중도 그래프</S.GraphTitle>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+        barSize={20}
+      >
+        <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Bar dataKey="집중도" fill="#8884d8" background={{ fill: "#eee" }} />
+      </BarChart>
     </S.GraphBox>
   );
 }
